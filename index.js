b4e577748146695eb9f02fa1301db1f2e198d540
Hassan.moaa <hassanmoataz500@outlook.com>
2024-03-09 21:47:41 +0200

    updated project files

--- index.js
+++ index.js
@@ -2,25 +2,15 @@ const showBtnEl = document.querySelectorAll('#showBtn');
 const hideBtnEl = document.querySelectorAll('#hideBtn');
 const paragraphEl = document.querySelectorAll('p');
 
-// showBtnEl.addEventListener('click', () => {
-// 	if (paragraphEl.style.display === 'none') {
-// 		paragraphEl.style.display = 'block';
-// 	} else {
-// 		paragraphEl.style.display = 'none';
-// 	}
-// 	showBtnEl.style.display = 'none';
-// 	hideBtnEl.style.display = 'block';
-// });
+showBtnEl.addEventListener('click', () => {
+	paragraphEl.style.display =
+		paragraphEl.style.display === 'none' ? 'block' : 'none';
+});
 
-// hideBtnEl.addEventListener('click', () => {
-// 	if (paragraphEl.style.display === 'block') {
-// 		paragraphEl.style.display = 'none';
-// 	} else {
-// 		paragraphEl.style.display = 'block';
-// 	}
-// 	hideBtnEl.style.display = 'none';
-// 	showBtnEl.style.display = 'block';
-// });
+hideBtnEl.addEventListener('click', () => {
+	paragraphEl.style.display =
+		paragraphEl.style.display === 'block' ? 'none' : 'block';
+});
 
 // function toggleDisclosure() {
 //     const isExpanded = this.getAttribute('aria-expanded') === 'true';
