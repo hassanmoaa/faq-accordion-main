7e582f526707bd1b2748b1b6fb9cbb635e6061c4
Hassan.moaa <hassanmoataz500@outlook.com>
2024-03-09 21:09:01 +0200

    Added Project Files

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ const showBtnEl = document.querySelectorAll('#showBtn');
 const hideBtnEl = document.querySelectorAll('#hideBtn');
 const paragraphEl = document.querySelectorAll('p');
 
-showBtnEl.addEventListener('click', () => {
-	if (paragraphEl.style.display === 'none') {
-		paragraphEl.style.display = 'block';
-	} else {
-		paragraphEl.style.display = 'none';
-	}
-	showBtnEl.style.display = 'none';
-	hideBtnEl.style.display = 'block';
-});
+// showBtnEl.addEventListener('click', () => {
+// 	if (paragraphEl.style.display === 'none') {
+// 		paragraphEl.style.display = 'block';
+// 	} else {
+// 		paragraphEl.style.display = 'none';
+// 	}
+// 	showBtnEl.style.display = 'none';
+// 	hideBtnEl.style.display = 'block';
+// });
 
-hideBtnEl.addEventListener('click', () => {
-	if (paragraphEl.style.display === 'block') {
-		paragraphEl.style.display = 'none';
-	} else {
-		paragraphEl.style.display = 'block';
-	}
-	hideBtnEl.style.display = 'none';
-	showBtnEl.style.display = 'block';
-});
+// hideBtnEl.addEventListener('click', () => {
+// 	if (paragraphEl.style.display === 'block') {
+// 		paragraphEl.style.display = 'none';
+// 	} else {
+// 		paragraphEl.style.display = 'block';
+// 	}
+// 	hideBtnEl.style.display = 'none';
+// 	showBtnEl.style.display = 'block';
+// });
 
 // function toggleDisclosure() {
 //     const isExpanded = this.getAttribute('aria-expanded') === 'true';
